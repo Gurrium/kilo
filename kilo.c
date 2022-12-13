81db526557131ccb5862401648866fa1bbe5f22f
gurrium <gurrium@hatena.ne.jp>
2022-12-13 22:27:40 +0900

    next: https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html#error-handling

--- kilo.c
+++ kilo.c
@@ -6,34 +6,36 @@
 
 struct termios orig_termios;
 
-void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
 
 void enableRawMode() {
-    tcgetattr(STDERR_FILENO, &orig_termios);
-    atexit(disableRawMode);
+  tcgetattr(STDERR_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= ~(CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-    enableRawMode();
+  enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
+  }
 
-    return 0;
+  return 0;
 }
