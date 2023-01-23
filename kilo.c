cbf2883f32ef5f97ab969e0495f5e2535ab4fb27
gurrium <gurrium@hatena.ne.jp>
2023-01-23 22:42:13 +0900

    next: https://viewsourcecode.org/snaptoken/kilo/05.aTextEditor.html#simple-backspacing

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k)&0x1f)
 
@@ -398,13 +399,24 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeyPress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
 
   switch (c) {
     case '\r':
       // TODO
       break;
+
     case CTRL_KEY('q'):
+      if (E.dirty && quit_times > 0) {
+        editorSetStatusMessage(
+            "WARNING!!! File has unsavd changes. "
+            "Press Ctrl-Q %d more times to quit.",
+            quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
 
@@ -456,6 +468,8 @@ void editorProcessKeyPress() {
       editorInsertChar(c);
       break;
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** output ***/
